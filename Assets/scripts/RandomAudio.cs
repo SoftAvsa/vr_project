94cfbd0e452d80e450fa04764495d6a9bd0e2911
SoftAvsa <ashenwan@gmail.com>
2022-06-20 16:47:00 +0200

    added sounds, fixed clock

---
+++ Assets/scripts/RandomAudio.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class RandomAudio : MonoBehaviour
+{
+    public float min_wait;
+    public float max_wait;
+    private float timer;
+    private float randomNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        randomNumber = Random.Range(min_wait, max_wait);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= randomNumber)
+        {
+            timer = 0;
+            //play thing
+            this.GetComponent<AudioSource>().Play();
+            randomNumber = Random.Range(min_wait, max_wait);
+        }
+    }
+}
