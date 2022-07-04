eff113515371d43d17265b48b88d22ff8a7ecd81
SoftAvsa <ashenwan@gmail.com>
2022-07-04 14:43:16 +0200

    test button script

--- Assets/general_timer.cs
+++ Assets/general_timer.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class general_timer : MonoBehaviour
 {
+    List<UnityEngine.XR.InputDevice> inputDevices = new List<UnityEngine.XR.InputDevice>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+        }
     }
 }
